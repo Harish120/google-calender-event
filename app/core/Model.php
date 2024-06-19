cbaabf523199ef047ea27c85322638ec63843e3b
Harish Pant <harish.pant@sociair.com>
2024-06-19 23:15:22 +0545

    :hammer_and_wrench: Add base Model class for database connection handling

---
+++ app/core/Model.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Core;
+
+use PDO;
+
+class Model
+{
+    protected $db;
+
+    public function __construct()
+    {
+        $config = require __DIR__ . '/../../config/config.php';
+        $this->db = new PDO(
+            'mysql:host=' . $config['db']['host'] . ';dbname=' . $config['db']['dbname'],
+            $config['db']['user'],
+            $config['db']['pass']
+        );
+    }
+}
